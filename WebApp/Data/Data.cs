047461d1839d2a0b507c5d7f38e1bcd7aa1a0379
Vitalij Gurin <vitalg16@gmail.com>
2021-04-11 18:29:26 +0300

    Added WebPage

---
+++ WebApp/Data/Data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Data
+{
+    public static class Data
+    {
+        private static Dictionary<string, Leaderboard> data;
+
+        public static Dictionary<string, Leaderboard> returnData()
+        {
+            if (data != null)
+            {
+                if (data.Count > 0)
+                {
+                    return data;
+                }
+            }
+            return new Dictionary<string, Leaderboard>();
+        }
+
+        public static void saveData(Dictionary<string, Leaderboard> Data)
+        {
+            data = Data;
+        }
+    }
+}
---
+++ WebApp/Data/LeaderboardService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Data
+{
+    public class LeaderboardService
+    {
+        private Dictionary<string, Leaderboard> data = new Dictionary<string, Leaderboard>();
+
+        public void getData()
+        {
+            while(data.Count < 1)
+            {
+                data = Data.returnData();
+            }
+        }
+
+        public Task<List<Leaderboard>> GetLeaderboardAsync()
+        {
+            getData();
+            List<Leaderboard> lb = data.Values.ToList();
+
+            lb = lb.OrderByDescending(item => item.score).ToList();
+
+            return Task.FromResult(lb);
+        }
+
+       
+    }
+}
